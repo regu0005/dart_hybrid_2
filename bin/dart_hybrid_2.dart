6a8930e27bdf3388c95795bf4b289f926a35bb07
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-09-27 19:14:30 -0400

    Arguments received from the command line

--- bin/dart_hybrid_2.dart
+++ bin/dart_hybrid_2.dart
@@ -7,12 +7,29 @@ void main(List<String> arguments) {
 
   print('============================================');
 
+  // 1. First part: Accesing to the class Lucky and printing the random number generated
   Lucky lucky = Lucky();
   int result = lucky.randomNumber();
 
   print('');
   print("The lucky number between ${lucky.min} and ${lucky.max} is: $result");
   print('');
+
+  print('============================================');
+
+  // 2. Second part: Reading the arguments received from the command line
+  if (arguments.isNotEmpty) {
+    var argument = '';
+
+    for (argument in arguments) {
+      var argumentLowerCase = argument.toLowerCase();
+      var argumentFixed =
+          "${argumentLowerCase[0].toUpperCase()}${argumentLowerCase.substring(1)}";
+      print('Argument: $argumentFixed');
+    }
+  } else {
+    print('No arguments');
+  }
 
   print('============================================');
 }
